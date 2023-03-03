0193831434f697273a9fee8e998c6611be357ea8
chocryp <126106122+chocryp@users.noreply.github.com>
2023-03-03 23:36:04 +0200

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -240,7 +240,6 @@ function updatePresaleStats() {
   }).catch((error) => {});
 
     chocoPresale.methods.pending().call().then((res) => {
-    console.res("pending chock", res)
     res = web3.utils.fromWei(res, "ether");
     res = parseFloat(Math.floor(res * 1000) / 1000).toFixed(0) 
     pendingchoc.innerHTML = res;
